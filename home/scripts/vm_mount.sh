8c5665f2dfc54ddb0b8594e3413f2e0da0727aa2
udf <tabhooked@gmail.com>
2018-12-15 00:42:41 +0200

    add  stuff to mount vm exernal drive

---
+++ home/scripts/vm_mount.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+# This script mounts my vm's disk image with vdfuse, the crypt partition with cryptsetup, and finally the actual partition.
+
+set -e
+
+# mount vdi image
+echo "Mounting image..."
+sudo vdfuse -f "/vm/drive/btw i use arch/btw i use arch.vdi" /vm/img
+
+# map encrypted partition to decrypt it
+echo "Mapping luks partition..."
+sudo cryptsetup open --type luks /vm/img/Partition2 cryptvm
+
+# mount root partition
+echo "Mounting root..."
+sudo mount /dev/mapper/cryptvm /vm/root
+
+# mount boot partition
+echo "Mounting boot..."
+sudo mount /vm/img/Partition1 /vm/root/boot
