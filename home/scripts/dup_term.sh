b167a4e046d6f5245f1af9d42468e886d1db4422
udf <tabhooked@gmail.com>
2018-12-26 16:47:58 +0200

    fix dup_term when directory has spaces

--- home/scripts/dup_term.sh
+++ home/scripts/dup_term.sh
@@ -6,6 +6,6 @@ child=$(pgrep -P $pid | head -n 1)
 [[ ! -z $child ]] && pid=$child
 
 # run a terminal in the target's current directory
-cd $(readlink -en "/proc/$pid/cwd")
+cd "$(readlink -en "/proc/$pid/cwd")"
 exec $TERMINAL
 
