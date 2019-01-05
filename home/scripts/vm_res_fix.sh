82efe185c66bd9c6d07f62e94613bcf0170fae98
udf <tabhooked@gmail.com>
2019-01-05 17:54:34 +0200

    fix host ip because vm is bridge now

--- home/scripts/vm_res_fix.sh
+++ home/scripts/vm_res_fix.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 DESKTOP_KEY="AAAAC3NzaC1lZDI1NTE5AAAAIF0496p9x/zRn/f2ePFOV5H7xjlBltqQmVsZ6kgbM6pc" 
-ssh-keyscan -t ed25519 10.0.2.2 | grep $DESKTOP_KEY && export VM_IS_HOME=1
+ssh-keyscan -t ed25519 192.168.2.2 | grep $DESKTOP_KEY && export VM_IS_HOME=1
 
 if [[ -v VM_IS_HOME ]]; then
     xrandr --output VGA-1 --mode desktophost
