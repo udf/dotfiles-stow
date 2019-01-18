4259d067d4a9c7ee8af848d9059874e88a8df07e
udf <tabhooked@gmail.com>
2019-01-18 23:10:56 +0200

    add script to download packages for all systems

---
+++ home/scripts/dl_packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+LIST_PACKAGES='pacman -Qq | grep -vx "$(pacman -Qqm)"'
+PACKAGE_LIST=~/packs.txt
+
+if [[ -v VM_IS_HOME ]]; then
+    eval $LIST_PACKAGES > $PACKAGE_LIST
+    ssh sam@192.168.2.4 "$LIST_PACKAGES" >> $PACKAGE_LIST
+    ssh sam@192.168.2.2 "$LIST_PACKAGES" >> $PACKAGE_LIST
+    sort $PACKAGE_LIST | uniq > /tmp/packs.txt
+    mv /tmp/packs.txt $PACKAGE_LIST
+    echo "collected $(wc -l < $PACKAGE_LIST) package names"
+else
+    sudo pacman -Syw $(cat $PACKAGE_LIST)
+fi
