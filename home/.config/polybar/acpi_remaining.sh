8c969bf109c9692f3d7fbcf03a6a14953db318e6
Sameer Hoosen <mohamedh@bbd.co.za>
2019-02-11 08:43:36 +0000

    display nothing when status is unknown

--- home/.config/polybar/acpi_remaining.sh
+++ home/.config/polybar/acpi_remaining.sh
@@ -5,6 +5,8 @@ do
         s/.+ (.+) remaining/%{u#ffda00}\1/;
         s/.+ (.+) until charged/%{u#00ff00}\1/
         s/.+ Full.+//
+        s/.+charging at zero rate.+//
+        s/.+Unknown.+//
     "
     sleep 3
 done
