9dcb0aaf176b3bddf0ba1d4723d9416b083e9a90
udf <tabhooked@gmail.com>
2019-03-31 17:28:26 +0200

    fix cd possibly failing and $TERMINAL globbing

--- home/scripts/dup_term.sh
+++ home/scripts/dup_term.sh
@@ -3,9 +3,8 @@ pid=$(xdotool getactivewindow getwindowpid)
 
 # Use first child, if we have one
 child=$(pgrep -P $pid | head -n 1)
-[[ ! -z $child ]] && pid=$child
+[[ -n $child ]] && pid=$child
 
 # run a terminal in the target's current directory
-cd "$(readlink -en "/proc/$pid/cwd")"
-exec $TERMINAL
-
+cd "$(readlink -en "/proc/$pid/cwd")" || exit 1
+exec "$TERMINAL"
