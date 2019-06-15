147868a52f0429ae455cd1c1a5f5c6ce26d12ab0
udf <tabhooked@gmail.com>
2019-06-15 21:10:08 +0200

    fox

--- home/scripts/vm_res_fix.sh
+++ home/scripts/vm_res_fix.sh
@@ -1,8 +1,4 @@
 #!/bin/zsh
 
-if [[ -v VM_IS_HOME ]]; then
-    xrandr --output VGA-1 --mode desktophost
-    xrandr --output Virtual-1 --mode desktophost
-else
-    xrandr --output VGA-1 --mode 1920x1080
-fi
+xrandr --output VGA-1 --mode 1080p
+xrandr --output Virtual-1 --mode 1080p
