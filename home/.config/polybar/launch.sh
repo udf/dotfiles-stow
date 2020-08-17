7d52da7a37e2349b8aa0a8450162d041eff27c67
udf <tabhooked@gmail.com>
2020-08-17 22:23:20 +0200

    Generate ramp variables from launch script

--- home/.config/polybar/launch.sh
+++ home/.config/polybar/launch.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # Basic script to kill all old bars and launch new.
 
+# Generate ramp variables
+ramp=('#66cc66' '#83cc66' '#a0cc66' '#bdcc66' '#ccbd66' '#cca066' '#cc8366' '#cc6666')
+
+for i in ${!ramp[@]}; do
+  export RAMP_$i="${ramp[$i]}"
+  export URAMP_$i="%{u${ramp[$i]}}"
+  export FRAMP_$i="%{F${ramp[$i]}}"
+done
+
 # Terminate already running bad instances
 killall -q polybar
 
