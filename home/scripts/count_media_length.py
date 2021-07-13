946bedd2ac63c11166d881f1958ce261276788cb
udf <tabhooked@gmail.com>
2021-07-13 17:17:28 +0200

    speedup media length counter

--- home/scripts/count_media_length.py
+++ home/scripts/count_media_length.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import os
+from concurrent.futures import ThreadPoolExecutor
+
 import ffmpeg
 
 
@@ -9,22 +11,28 @@ def walk_files(path):
       yield os.path.join(root, file)
 
 
+def probe_media_length(path):
+  try:
+    d = ffmpeg.probe(path)
+    if 'duration' not in d['format']:
+      return 0
+    return float(d['format']['duration'])
+  except ffmpeg.Error as e:
+    # this is fine
+    pass
+  return 0
+
+
 print('Gathering files...')
 files = list(walk_files('.'))
 num_files = len(files)
 
 print('Probing...')
 total_len = 0
-for i, f in enumerate(files):
-  print(f'\r{round((i + 1) / num_files * 100, 2)}% ({i + 1}/{num_files})', end='')
-  try:
-    d = ffmpeg.probe(f)
-    if 'duration' not in d['format']:
-      continue
-    total_len += float(d['format']['duration'])
-  except ffmpeg.Error as e:
-    # this is fine
-    pass
+with ThreadPoolExecutor() as pool:
+  for i, length in enumerate(pool.map(probe_media_length, files)):
+    print(f'\r{round((i + 1) / num_files * 100, 2):.2f}% ({i + 1}/{num_files})', end='')
+    total_len += length
 
 print('\r', end='')
 print(f'{round(total_len / 3600, 2)} hours ({round(total_len / 3600 / 24, 2)} days)')
