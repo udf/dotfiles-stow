c0638cf8c757eac3e8e5d0361071ad8b816db4db
udf <tabhooked@gmail.com>
2021-07-24 20:18:04 +0200

    Use GTK file dialog (for the millionth time), get cursors from system dir

--- home/.config/nixpkgs/overlays/tdesktop-udf-patched.nix
+++ home/.config/nixpkgs/overlays/tdesktop-udf-patched.nix
@@ -1,21 +1,5 @@
 
 self: super:
-let
-  defaultCursor = (self.callPackage ({ stdenv }: stdenv.mkDerivation {
-    name = "global-cursor-theme";
-    unpackPhase = "true";
-    outputs = [ "out" ];
-    installPhase = ''
-      mkdir -p $out/share/icons/default
-      cat << EOF > $out/share/icons/default/index.theme
-      [Icon Theme]
-      Name=Default
-      Comment=Default Cursor Theme
-      Inherits=Adwaita
-      EOF
-    '';
-  }) {});
-in
 {
   tdesktop = super.tdesktop.overrideAttrs (oldAttrs: rec {
     patches = (oldAttrs.patches or []) ++ [
@@ -30,12 +14,11 @@ in
       wrapProgram $out/bin/telegram-desktop \
         "''${gappsWrapperArgs[@]}" \
         "''${qtWrapperArgs[@]}" \
-        --set TDESKTOP_USE_GTK_FILE_DIALOG 1 \
+        --set XDG_CURRENT_DESKTOP Unity \
         --set QT_XCB_GL_INTEGRATION none \
         --prefix XDG_DATA_DIRS : ${super.arc-theme}/share \
         --prefix XDG_DATA_DIRS : ${super.arc-icon-theme}/share \
-        --prefix XCURSOR_PATH : ${super.gnome3.adwaita-icon-theme}/share/icons \
-        --prefix XCURSOR_PATH : ${defaultCursor}/share/icons
+        --prefix XCURSOR_PATH : /usr/share/icons
     '';
   });
 }
