671bf649683572cab522fb4ed1b381b17face689
udf <tabhooked@gmail.com>
2021-11-27 20:41:12 +0200

    add backup script

---
+++ home/scripts/backup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+if [[ $EUID -ne 0 ]]; then
+   echo 'This script must be run as root'
+   exit 1
+fi
+
+if zfs list backup; then
+   echo 'Pool already imported, continuing...'
+else
+   echo 'Pool not found, trying to import'
+   zpool import backup
+   WAS_IMPORTED=1
+fi
+
+if mountpoint /backup/root; then
+   echo 'Dataset mounted, continuing...'
+else
+   echo 'Loading key and mounting dataset...'
+   zfs load-key backup/root
+   zfs mount backup/root
+fi
+
+zfs list backup/root
+
+echo 'Syncing root...'
+rsync -aAXHxx --delete --info=progress2 --exclude={/dev,/proc,/sys,/tmp,/run,/lost+found,/nix,/home/sam/Downloads/seedbox,/home/sam/.cache/{fontconfig,mozilla}} / /backup/root/
+
+echo 'Snapshotting...'
+zfs snapshot "backup/root@$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
+zfs list -t snapshot backup/root
+
+if [[ "$WAS_IMPORTED" == 1 ]]; then
+   echo 'Exporting pool...'
+   zpool export backup
+else
+   echo 'Not exporting pool because we did not import it.'
+fi
