15675d5808b738168d1c4857600ed75728312a5e
udf <tabhooked@gmail.com>
2021-12-05 14:34:49 +0200

    i are programmer

--- home/.config/mpv/scripts/current-chapter.js
+++ home/.config/mpv/scripts/current-chapter.js
@@ -1,8 +1,8 @@
 function chapter_change(_, meta) {
-  if (meta) {
-    var title = meta.TITLE || meta.title;
-    print(title)
-  }
+  if (!meta) return;
+  var title = meta.TITLE || meta.title;
+  if (!title) return;
+  print(title)
 }
 
 mp.observe_property('chapter-metadata', 'native', chapter_change);
