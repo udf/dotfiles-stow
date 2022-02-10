20ae0ab386d49543b0e2b6b05d0d799784ed59ee
udf <tabhooked@gmail.com>
2022-02-11 00:59:08 +0200

    delete disliked from yt dir

--- home/scripts/mpd-genfavs.py
+++ home/scripts/mpd-genfavs.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
+from pathlib import Path
 from mpd import CommandError
 from _mpd import client
 client.timeout = 10
+client.idletimeout = 10
 
+music_dir = Path('/booty/media/music')
 PLAYLIST_NAME = 'all_favs'
 
 try:
   client.rm(PLAYLIST_NAME)
 except CommandError:
   pass
+
+for track in client.sticker_find('song', 'favourites', 'rating', '=', 2):
+  p = (music_dir / track['file'])
+  print('deleting', p)
+  p.unlink()
+
+client.update('favourites')
+client.idle('update')
 
 for track in client.sticker_find('song', '', 'rating', '=', 10):
   client.playlistadd(PLAYLIST_NAME, track['file'])
