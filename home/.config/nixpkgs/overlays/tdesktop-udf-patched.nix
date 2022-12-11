777aafb490735838ed149c1fcaa493941da99245
udf <tabhooked@gmail.com>
2022-12-11 16:23:08 +0200

    thanks mr bob

--- home/.config/nixpkgs/overlays/tdesktop-udf-patched.nix
+++ home/.config/nixpkgs/overlays/tdesktop-udf-patched.nix
@@ -7,15 +7,16 @@ let
   '';
 in
 {
+  # tdesktop-custom = {...}; ????????
   tdesktop = super.tdesktop.overrideAttrs (oldAttrs: rec {
     patches = (oldAttrs.patches or []) ++ [
-      ./tdesktop-udf-patches/always_delete_for_everyone.patch
       ./tdesktop-udf-patches/always_clear_history_for_everyone.patch
       ./tdesktop-udf-patches/always_pin_without_notify.patch
       ./tdesktop-udf-patches/always_send_as_photo_or_album.patch
       ./tdesktop-udf-patches/use_tagbot_for_gifs_search.patch
     ];
 
+    # TODO: move this into separate derivation
     postFixup = ''
       wrapProgram $out/bin/telegram-desktop \
         --argv0 'telegram-desktop' \
