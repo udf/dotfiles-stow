3fec173a250352e7607868efc39ab18d0ea0b415
udf <tabhooked@gmail.com>
2023-04-25 00:26:18 +0200

    add print option

--- home/scripts/mpd-q-favs.py
+++ home/scripts/mpd-q-favs.py
@@ -8,20 +8,28 @@ root = Path('/booty/media/music')
 
 
 def queue_rated(d, rating):
-  for track in client.sticker_find('song', d, 'rating', '=', rating):
-    client.add(track['file'])
+  i = 0
+  for i, track in enumerate(client.sticker_find('song', d, 'rating', '=', rating)):
+    add_fn(track['file'])
+  if not args.print:
+    print(f'Queued {i + 1} track(s)')
 
 
 def queue_unrated(d):
   to_files = lambda l: {t['file'] for t in l}
   rated = to_files(client.sticker_find('song', d, 'rating', '>', 0))
   all_tracks = to_files(client.find(f"(base '{d}')"))
-  for track in sorted(all_tracks - rated):
-    client.add(track)
+  print(f'{len(all_tracks)} tracks, {len(rated)} rated')
+  i = 0
+  for i, track in enumerate(sorted(all_tracks - rated)):
+    add_fn(track)
+  if not args.print:
+    print(f'Queued {i + 1} track(s)')
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('dirs', action='extend', nargs='+')
+parser.add_argument('--print', action='store_true')
 modes = parser.add_mutually_exclusive_group()
 modes.add_argument('--unrated', action='store_true')
 modes.add_argument('--disliked', action='store_true')
@@ -32,6 +40,8 @@ if args.disliked:
   f = lambda d: queue_rated(d, 2)
 if args.unrated:
   f = queue_unrated
+
+add_fn = print if args.print else client.add
 
 for d in args.dirs:
   d = Path(d)
