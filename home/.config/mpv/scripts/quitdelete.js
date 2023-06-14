5323f6935bb2c6ff7862f26a0bc504e8114b645e
udf <tabhooked@gmail.com>
2023-06-14 16:43:45 +0200

    allow unmarking and use trash instead of rm

--- home/.config/mpv/scripts/quitdelete.js
+++ home/.config/mpv/scripts/quitdelete.js
@@ -10,7 +10,7 @@ function dump(v) {
 }
 
 function quit_delete() {
-  var file_list = Object.keys(files);
+  var file_list = Object.keys(files).filter(function (k) { return files[k]; });
 
   print('Deleting ' + file_list.length + ' file(s)!')
   var sub_list = [];
@@ -20,20 +20,34 @@ function quit_delete() {
     sub_list.push(file);
     if (sub_list.length >= 16) {
       mp.utils.subprocess_detached({
-        args: ['rm'].concat(sub_list)
+        args: ['send2trash'].concat(sub_list)
       });
       sub_list = [];
     }
   });
   if (sub_list.length > 0) {
     mp.utils.subprocess_detached({
-      args: ['rm'].concat(sub_list)
+      args: ['send2trash'].concat(sub_list)
     });
   }
 }
 
 function file_loaded() {
-  files[mp.get_property_native('path')] = true;
+  var k = mp.get_property_native('path');
+  if (!files[k]) {
+    mp.osd_message('Marked for delete');
+    files[k] = true;
+  }
+}
+
+function toggle_mark() {
+  var k = mp.get_property_native('path')
+  files[k] = !files[k];
+  if (files[k]) {
+    mp.osd_message('Marked for delete');
+  } else {
+    mp.osd_message('Unmarked for delete');
+  }
 }
 
 mp.options.read_options(opts);
@@ -42,3 +56,5 @@ if (opts.enabled !== '') {
   mp.register_event('shutdown', quit_delete);
   mp.register_event('file-loaded', file_loaded);
 }
+
+mp.add_key_binding('ctrl+z', 'toggle-mark', toggle_mark);
