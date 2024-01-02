df229f6575c9037367a56aab06d653e09981c767
udf <tabhooked@gmail.com>
2024-01-02 21:22:34 +0200

    8 > 10
    i hate strings

--- home/scripts/mpd-gen-nearfavs.py
+++ home/scripts/mpd-gen-nearfavs.py
@@ -5,8 +5,12 @@ from _mpd import client
 
 to_file_set = lambda l: {t['file'] for t in l}
 
-favourites = to_file_set(client.sticker_find('song', 'albums', 'rating', '>', 8))
-fav_dupes = to_file_set(client.sticker_find('song', 'albums', 'rating', '>', 4))
+favourites = (
+  to_file_set(client.sticker_find('song', 'albums', 'rating', '=', 8))
+  | to_file_set(client.sticker_find('song', 'albums', 'rating', '=', 10))
+)
+fav_dupes = to_file_set(client.sticker_find('song', 'albums', 'rating', '=', 4))
+print(f'{len(favourites)} real likes, {len(fav_dupes)} duplicate likes')
 liked = favourites | fav_dupes
 rated = to_file_set(client.sticker_find('song', 'albums', 'rating', '>', 0))
 all_tracks = to_file_set(client.find("(base 'albums')"))
