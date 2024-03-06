715215cbe7f34876cf9877cdc117c48c7134a6e7
udf <tabhooked@gmail.com>
2024-03-06 22:55:27 +0200

    generate near loved and near favs

--- home/scripts/mpd-gen-nearfavs.py
+++ home/scripts/mpd-gen-nearfavs.py
@@ -5,23 +5,28 @@ from _mpd import client
 
 to_file_set = lambda l: {t['file'] for t in l}
 
-favourites = (
-  to_file_set(client.sticker_find('song', 'albums', 'rating', '=', 8))
-  | to_file_set(client.sticker_find('song', 'albums', 'rating', '=', 10))
-)
-fav_dupes = to_file_set(client.sticker_find('song', 'albums', 'rating', '=', 4))
-print(f'{len(favourites)} real likes, {len(fav_dupes)} duplicate likes')
-liked = favourites | fav_dupes
 rated = to_file_set(client.sticker_find('song', 'albums', 'rating', '>', 0))
 all_tracks = to_file_set(client.find("(base 'albums')"))
 unrated = all_tracks - rated
 
-print(f'{len(all_tracks)} tracks, {len(rated)} rated, {len(unrated)} unrated, {len(liked)} liked')
+print(f'{len(all_tracks)} tracks, {len(rated)} rated, {len(unrated)} unrated')
 
-dirs_with_liked = {str(Path(p).parent) for p in liked}
-print(f'{len(dirs_with_liked)} dirs containing likes')
-near_favs = {p for p in unrated if str(Path(p).parent) in dirs_with_liked}
+def gen_near_playlist(name, src_set):
+  dirs_with_liked = {str(Path(p).parent) for p in src_set}
+  print(f'{name}: found {len(dirs_with_liked)} dirs')
+  near_favs = {p for p in unrated if str(Path(p).parent) in dirs_with_liked}
 
-print(f'{len(near_favs)} unrated files in dirs containing likes')
-with open('/booty/media/music/playlists/_near_favs.m3u', 'w') as f:
-  f.write('\n'.join(sorted(near_favs)))
+  print(f'{name}: {len(near_favs)} unrated files in dirs')
+  with open(f'/booty/media/music/playlists/{name}.m3u', 'w') as f:
+    f.write('\n'.join(sorted(near_favs)))
+
+
+favourites = to_file_set(client.sticker_find('song', 'albums', 'rating', '=', 8))
+loved = to_file_set(client.sticker_find('song', 'albums', 'rating', '=', 10))
+fav_dupes = to_file_set(client.sticker_find('song', 'albums', 'rating', '=', 4))
+print(f'{len(favourites)} likes, {len(loved)} loved, {len(fav_dupes)} duplicate likes')
+
+all_liked = favourites | loved | fav_dupes
+print(f'{len(favourites)} all likes')
+gen_near_playlist('_near_favs', all_liked)
+gen_near_playlist('_near_loved', loved)
