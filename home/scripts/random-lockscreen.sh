167d91499e6c962456659de4a4f66dd71948a765
udf <tabhooked@gmail.com>
2024-04-09 03:34:36 +0200

    thanks picom

--- home/scripts/random-lockscreen.sh
+++ home/scripts/random-lockscreen.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 WALLPAPER_DIR="$HOME/Documents/$(hostname)-lock"
+PICOM=0
 
 while read line 
 do
@@ -8,5 +9,13 @@ do
     WALLPAPER="$(find "$WALLPAPER_DIR" -type f | shuf -n 1)"
     echo "Changing lockscreen to $WALLPAPER"
     cp "$WALLPAPER" /usr/share/backgrounds/lockscreen.png
+    if pkill picom; then
+      PICOM=1
+    fi
+  fi
+  if [[ $line =~ "Session.Unlock" ]]; then
+    if [ -n "$PICOM" ]; then
+      picom &!
+    fi
   fi
 done < <(gdbus monitor --system --dest org.freedesktop.login1)
