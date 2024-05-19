66974892b80274161dbce0bc0c1b397ff8e79611
udf <tabhooked@gmail.com>
2024-05-19 22:03:24 +0200

    update yt file format
    these people are the worst
    yt-dlp/yt-dlp/commit/91ebc64068d2957400dbfaadff23e995392b3963

--- home/.config/mpv/scripts/open-yt.js
+++ home/.config/mpv/scripts/open-yt.js
@@ -1,11 +1,12 @@
 function open_yt_in_browser() {
   var filepath = mp.get_property_native('path');
-  var videoId = filepath.match(/-([\dA-Za-z_-]{11})\..+$/);
+  var videoId = filepath.match(/-([\dA-Za-z_-]{11})\./) || filepath.match(/ \[([\dA-Za-z_-]{11})\]\./);
   if (!videoId) {
     mp.osd_message('No videoId found');
     return;
   }
   mp.command_native(['run', 'xdg-open', 'https://youtu.be/' + videoId[1]])
+  mp.osd_message('Opened Youtube url in browser');
 }
 
 mp.add_key_binding('c', 'open-yt-in-browser', open_yt_in_browser);
