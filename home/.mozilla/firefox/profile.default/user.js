b488ebc87c13900b249ee44df52a90cab9e26d23
udf <tabhooked@gmail.com>
2024-07-16 22:03:16 +0200

    mozzarella filofax

--- home/.mozilla/firefox/profile.default/user.js
+++ home/.mozilla/firefox/profile.default/user.js
@@ -32,6 +32,31 @@ user_pref("full-screen-api.warning.timeout", 0);
 // tracking shit
 user_pref("security.ssl.disable_session_identifiers", true);
 
+// mozilla is my favourite ad company
+user_pref("dom.private-attribution.submission.enabled", false);
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.ping-centre.telemetry", false);
+user_pref("datareporting.healthreport.service.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("datareporting.sessions.current.clean", true);
+user_pref("devtools.onboarding.telemetry.logged", false);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.hybridContent.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.prompted", 2);
+user_pref("toolkit.telemetry.rejected", true);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.server", "");
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.unifiedIsOptIn", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+
 // go fast
 user_pref("gfx.webrender.all", true);
 user_pref("layers.acceleration.force-enabled", true);
