7d195452757ca365703d77b9973beccdd2ccc501
udf <tabhooked@gmail.com>
2024-11-04 17:49:11 +0200

    fix backup script; move music to own dataset
    bet a certain poser doesn't understand the reason for datasets

--- home/scripts/backup.sh
+++ home/scripts/backup.sh
@@ -25,18 +25,27 @@ fi
 zfs list backup/root
 
 if [ "$1" = 'mount' ]; then
-   echo 'Not syncing because mount was passed'
-   exit
+  echo 'Not syncing because mount was passed'
+  exit
 fi
 
 echo 'Syncing root...'
-rsync -aAXHxx --delete --info=progress2 --exclude={/dev,/proc,/sys,/tmp,/run,/lost+found,/nix/store,/home/sam/Downloads,/home/sam/.cache,/home/sam/.local/share/nicotine,/var/lib/libvirt/images} / /backup/root/
+rsync -vaAXHx --delete --info=progress2 --exclude={/dev,/proc,/sys,/tmp,/run,/lost+found,/nix/store,/home/sam/.cache,/home/sam/.local/share/nicotine,/var/lib/libvirt/images,/var/lib/docker} / /backup/root/
 echo 'Syncing boot...'
-rsync -aAXHxx --delete --info=progress2 /boot/ /backup/root/boot/
-
-echo 'Snapshotting...'
+rsync -vaAXHx --delete --info=progress2 /boot/ /backup/root/boot/
+echo 'Snapshotting root backup...'
 zfs snapshot "backup/root@$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
 zfs list -t snapshot backup/root
+
+echo 'Syncing enc dataset...'
+syncoid --sendoptions="Rw" booty/enc backup/enc
+
+echo 'Syncing backups dataset...'
+syncoid --sendoptions="Rw" booty/misc/backups backup/backups
+
+echo 'Syncing music dataset...'
+systemctl --user -M sam@ start music_tasks.service
+syncoid --sendoptions="Rw" booty/music backup/music
 
 if [[ "$WAS_IMPORTED" == 1 ]]; then
   echo 'Exporting pool...'
