68cee3490ff01f9ee2004e8c515d5809b1d06987
udf <tabhooked@gmail.com>
2024-11-04 17:51:18 +0200

    move yt wl downloads to ananke
    i hate google

--- home/scripts/dl_yt_wl_pl.sh
+++ home/scripts/dl_yt_wl_pl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-cd ~/Downloads/durga_qbit/yt
+cd ~/Downloads/ananke/yt
 new_wl="$(~/.local/bin/yt-dlp --cookies-from-browser firefox --flat-playlist --print id 'https://www.youtube.com/playlist?list=WL')"
 if [ "$(<wl.txt md5sum)" = "$(echo -n "$new_wl" | md5sum)" ]; then
   echo "no new video IDs"
