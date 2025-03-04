5264e7ffb3e4ac9c55387257b29d8942f0b3bc7c
udf <tabhooked@gmail.com>
2025-03-05 00:01:51 +0200

    sync root to booty, then backup

--- home/scripts/backup.sh
+++ home/scripts/backup.sh
@@ -14,41 +14,53 @@ else
   WAS_IMPORTED=1
 fi
 
-if mountpoint /backup/root; then
-  echo 'Dataset mounted, continuing...'
-else
-  echo 'Loading key and mounting dataset...'
-  zfs load-key backup/root
-  zfs mount backup/root
-fi
-
-zfs list backup/root
-
 if [ "$1" = 'mount' ]; then
   echo 'Not syncing because mount was passed'
   exit
 fi
 
+BACKUP_DATASET=booty/misc/backups/root
 echo 'Syncing root...'
-rsync -vaAXHx --delete --info=progress2 --exclude={/dev,/proc,/sys,/tmp,/run,/lost+found,/nix/store,/home/sam/.cache,/home/sam/.local/share/nicotine,/home/sam/Downloads/phanes_ext,/home/sam/Downloads/qbit,/var/lib/libvirt/images,/var/lib/docker} / /backup/root/
+rsync -vaAXHx --delete --info=progress2 \
+  --exclude=/.beeshome \
+  --exclude=/dev \
+  --exclude=/proc \
+  --exclude=/sys \
+  --exclude=/tmp \
+  --exclude=/run \
+  --exclude=/lost+found \
+  --exclude=/nix/store \
+  --exclude=/home/sam/.config/syncthing \
+  --exclude=/home/sam/.cache \
+  --exclude=/home/sam/.local/share/Steam/steamapps/common \
+  --exclude=/home/sam/.local/share/nicotine \
+  --exclude=/home/sam/Downloads/phanes_ext \
+  --exclude=/home/sam/Downloads/durga_qbit \
+  --exclude=/home/sam/Downloads/qbit \
+  --exclude=/home/sam/Downloads/claire \
+  --exclude=/var/lib/libvirt/images \
+  --exclude=/var/lib/docker \
+  / "/$BACKUP_DATASET/"
 echo 'Syncing boot...'
-rsync -vaAXHx --delete --info=progress2 /boot/ /backup/root/boot/
+rsync -vaAXHx --delete --info=progress2 /boot/ "/$BACKUP_DATASET/boot/"
 echo 'Snapshotting root backup...'
-zfs snapshot "backup/root@$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
-zfs list -t snapshot backup/root
-
-echo 'Syncing enc dataset...'
-syncoid --sendoptions="Rw" booty/enc backup/enc
+zfs snapshot "$BACKUP_DATASET@$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
+echo 'Deleting old snapshots...'
+zfs list -t snapshot -o name -S creation $BACKUP_DATASET | grep -v '@syncoid' | tail -n +5 | xargs -rn 1 sudo zfs destroy -vr
+zfs list -t snapshot $BACKUP_DATASET
 
 echo 'Syncing backups dataset...'
-syncoid --sendoptions="Rw" booty/misc/backups backup/backups
+syncoid --recursive --sendoptions=w booty/misc/backups backup/backups
+
+echo 'Syncing enc dataset...'
+syncoid --recursive --sendoptions=w booty/enc backup/enc
 
 echo 'Syncing cached dataset...'
-syncoid --sendoptions="Rw" booty/cached backup/cached
+syncoid --recursive --sendoptions=w booty/cached backup/cached
 
 echo 'Syncing music dataset...'
 systemctl --user -M sam@ start music_tasks.service
-syncoid --sendoptions="Rw" booty/music backup/music
+syncoid --recursive --sendoptions=w booty/music backup/music
 
 if [[ "$WAS_IMPORTED" == 1 ]]; then
   echo 'Exporting pool...'
