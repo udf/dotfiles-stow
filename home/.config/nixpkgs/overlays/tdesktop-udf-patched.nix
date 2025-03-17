b438f477bbb655bc3d785c69fd6df432830c90e7
udf <tabhooked@gmail.com>
2025-03-17 23:36:35 +0200

    update tdesktop
    $100% bet some poser switched off this instead of learning basic nix, despite "administering" NixOS servers. actual skiddie smh

--- home/.config/nixpkgs/overlays/tdesktop-udf-patched.nix
+++ home/.config/nixpkgs/overlays/tdesktop-udf-patched.nix
@@ -1,23 +1,23 @@
-
 self: super:
 let
-  nixgl = import <nixgl> {};
-  nixgl-wrapper = self.writeShellScriptBin "nixgl-wrapper" ''
-    ${nixgl.auto.nixGLDefault}/bin/nixGL @PATH@
-  '';
+  # nixgl = import <nixgl> { };
+  # nixgl-wrapper = self.writeShellScriptBin "nixgl-wrapper" ''
+  #   ${nixgl.auto.nixGLDefault}/bin/nixGL @PATH@
+  # '';
   tdesktopPkg = super.tdesktop.override { stdenv = super.ccacheStdenv; };
 in
 {
-  tdesktop = tdesktopPkg.overrideAttrs (oldAttrs: rec {
-    patches = (oldAttrs.patches or []) ++ [
-      ./tdesktop-udf-patches/always_clear_history_for_everyone.patch
-      ./tdesktop-udf-patches/always_pin_without_notify.patch
-      ./tdesktop-udf-patches/always_send_sticker_as_file.patch
-      ./tdesktop-udf-patches/use_tagbot_for_gifs_search.patch
-    ];
+  tdesktop = tdesktopPkg.overrideAttrs (oldAttrs: {
+    unwrapped = oldAttrs.unwrapped.overrideAttrs (oldAttrs: {
+      patches = (oldAttrs.patches or [ ]) ++ [
+        ./tdesktop-udf-patches/always_clear_history_for_everyone.patch
+        ./tdesktop-udf-patches/always_pin_without_notify.patch
+        ./tdesktop-udf-patches/always_send_sticker_as_file.patch
+      ];
+    });
 
-    # TODO: move this into separate derivation
     postFixup = ''
+      ls $out/bin
       wrapProgram $out/bin/telegram-desktop \
         --argv0 'telegram-desktop' \
         "''${gappsWrapperArgs[@]}" \
@@ -26,11 +26,10 @@ in
         --prefix XDG_DATA_DIRS : /usr/share \
         --prefix XCURSOR_PATH : /usr/share/icons \
         --prefix LD_LIBRARY_PATH : ${self.xorg.libXcursor}/lib
-
-      mv $out/bin/telegram-desktop $out/bin/.telegram-desktop
-      cp ${nixgl-wrapper}/bin/nixgl-wrapper $out/bin/telegram-desktop
-      substituteInPlace $out/bin/telegram-desktop \
-        --replace '@PATH@' "$out/bin/.telegram-desktop"
     '';
+      # mv $out/bin/telegram-desktop $out/bin/.telegram-desktop
+      # cp ${nixgl-wrapper}/bin/nixgl-wrapper $out/bin/telegram-desktop
+      # substituteInPlace $out/bin/telegram-desktop \
+      #   --replace-fail '@PATH@' "$out/bin/.telegram-desktop"
   });
 }
